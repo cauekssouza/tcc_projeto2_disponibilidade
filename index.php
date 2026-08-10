2154a6538da30cd952b02406d9e3dd2d46a80e07
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2026-08-10 08:37:44 -0300

    tentativa 1 - Baixa

--- index.php
+++ index.php
@@ -5,34 +5,43 @@ session_start();
 require '../assets/includes/auth_functions.php';
 check_logged_in();
 
-
-
-if(isset($_COOKIE[session_name()])){
-
-    setcookie(session_name(), '', time()-7000000, '/');
-}
-
-if(isset($_COOKIE['rememberme'])) {
-
-    setcookie('rememberme', '', time()-7000000, '/');
-    
-    require '../assets/setup/db.inc.php';
-    $sql = "DELETE FROM auth_tokens WHERE user_email=? AND auth_type='remember_me';";
-    $stmt = mysqli_stmt_init($conn);
-    if (mysqli_stmt_prepare($stmt, $sql)){
-
-        mysqli_stmt_bind_param($stmt, "s", $_SESSION['email']);
-        mysqli_stmt_execute($stmt);
-        
-        if (isset($_SESSION['auth'])){
-
-            $_SESSION['auth'] = 'verified';
-        }
+// Função utilitária para remover cookies com segurança
+function clear_cookie(string $name): void {
+    if (isset($_COOKIE[$name])) {
+        setcookie($name, '', time() - 3600, '/', '', false, true);
     }
 }
 
+// Remove cookies de sessão e remember-me
+clear_cookie(session_name());
+clear_cookie('rememberme');
+
+// Se o usuário estava usando "remember me", remove o token do banco
+if (isset($_COOKIE['rememberme'])) {
+
+    require '../assets/setup/db.inc.php';
+
+    $sql = "DELETE FROM auth_tokens 
+            WHERE user_email = ? 
+              AND auth_type = 'remember_me'";
+
+    $stmt = mysqli_prepare($conn, $sql);
+
+    if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "s", $_SESSION['email']);
+        mysqli_stmt_execute($stmt);
+    }
+
+    // Mantém o estado de autenticação consistente
+    if (isset($_SESSION['auth'])) {
+        $_SESSION['auth'] = 'verified';
+    }
+}
+
+// Finaliza sessão
 session_unset();
 session_destroy();
 
+// Redireciona para login
 header("Location: ../login/");
 exit();
